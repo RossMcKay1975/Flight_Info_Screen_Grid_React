fde801d24b34fb386f4b7c8ebb3df6f935996535
Ross Mckay <ross.mckay1975@gmail.com>
2019-12-04 17:38:07 +0000

    add additional info

--- src/components/FlightsDetail.js
+++ src/components/FlightsDetail.js
@@ -10,15 +10,15 @@ class FlightsDetail extends Component {
                 <React.Fragment>
                 
                 <div className="grid-container">
-                        <div className="grid-item image"><img src={this.props.flight.Image} alt="airline" /></div>
-                        <div className="grid-item flightNo"> {this.props.flight.FlightNo} </div>
+                        <div className="grid-item image"><img src={this.props.flight.Image} alt="airline" />  {this.props.flight.FlightNo}</div>
+                        {/* <div className="grid-item flightNo">  </div> */}
                         <div className="grid-item date"> {this.props.flight.Date} </div>
                         <div className="grid-item time"> {this.props.flight.Time} </div>
                         <div className="grid-item arrDep"> {this.props.flight.ArrDep} </div>
                         <div className="grid-item POC"> {this.props.flight.PortOfCallA} </div>
                         <div className="grid-item status"> {this.props.flight.Status} </div>
-                        {/* <div className="grid-item otherInfo"> {this.props.flight.OtherInfo} </div> */}
-                        {/* <div className="grid-item add"> {this.props.flight.Additional} </div> */}
+                        <div className="grid-item otherInfo"> {this.props.flight.OtherInfo} </div>
+                        <div className="grid-item add"> {this.props.flight.Additional} </div>
                 </div>
 
                 </React.Fragment>
